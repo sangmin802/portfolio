fe88f4372075b8051e518398f0037c2b13c3595e
DESKTOP-LB3HQR1\USER <sangmin802@naver.com>
2021-03-17 16:19:40 +0900

    Updates

---
+++ precache-manifest.d617438bb4bf4efaa038011fe25a55a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60462b319208f6d30abb7a37301564cf",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b6dec1b08cfa0085b029",
+    "url": "/portfolio/static/css/main.7bdf7c30.chunk.css"
+  },
+  {
+    "revision": "89c91ce83205133001b0",
+    "url": "/portfolio/static/js/2.1d132ecd.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/portfolio/static/js/2.1d132ecd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6dec1b08cfa0085b029",
+    "url": "/portfolio/static/js/main.e1c4c154.chunk.js"
+  },
+  {
+    "revision": "5e4cd4c6f14baf3f4e46",
+    "url": "/portfolio/static/js/runtime-main.86f1c7e8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.2f9023d4d126ec720b83faf42f506c92.js"
+  "/portfolio/precache-manifest.d617438bb4bf4efaa038011fe25a55a0.js"
 );
 
 self.addEventListener('message', (event) => {
